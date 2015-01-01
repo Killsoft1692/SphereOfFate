c4295cbcf8529d24bda56f63cb15aaaa1e10bb7c
Ilin Serhiy <Killsoft1692@users.noreply.github.com>
2015-01-01 21:01:46 +0200

    Update Confirm.php
    add some fixes

--- Services/Confirm.php
+++ Services/Confirm.php
@@ -4,18 +4,15 @@ namespace Acme\SphereOfFateBundle\Services;
 
 class Confirm
 {
-    public function Random()
+    public function random()
     {
         $number = rand(1,100);
 
-        if($number<50){
-
+        if ($number<50) {
             $result = 'No, maybe tomorrow will be your day';
 
             return $result;
-        }
-        else{
-
+        } else {
             $result = 'Yes, today is your day';
 
             return $result;
